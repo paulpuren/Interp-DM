56d3bb9ac5b1fce252cb974038047558f32dbe22
paulpuren <ren.pu@northeastern.edu>
2025-08-13 11:40:42 -0700

    remove zoom in

--- src/plotting.py
+++ src/plotting.py
@@ -39,33 +39,33 @@ def plot_samples(
         axarr[1, i].set_yticks([])
         axarr[1, i].title.set_text(f"Interpolated (RFNE={error:.3f})")  
         
-        # Draw zoom in 
-        zoom_in_factor = 2
-        upscale_factor = 7.5
-        ec = ".2"
-        zoom_loc_x = (1000,1400)
-        zoom_loc_y = (500,100)
-        axins = zoomed_inset_axes(axarr[1,i], zoom_in_factor, loc = 'lower left')    
-        axins.imshow(samples[i, 0, :, :], cmap = cmocean.cm.balance)
-        axins.set_xlim(tuple(val // upscale_factor for val in zoom_loc_x))
-        axins.set_ylim(tuple(val // upscale_factor for val in zoom_loc_y))
-        plt.xticks(visible = False)
-        plt.yticks(visible = False)
-        _patch, pp1, pp2 = mark_inset(
-            axarr[1, i], 
-            axins, 
-            loc1 = 4, 
-            loc2 = 2, 
-            fc = "none", 
-            ec = ec, 
-            lw = 1.0, 
-            color = 'black'
-        ) 
-        #pp1.loc1, pp1.loc2 = 2, 3  # inset corner 2 to origin corner 3 (would expect 2)
-        #pp2.loc1, pp2.loc2 = 4, 1  # inset corner 4 to origin corner 1 (would expect 4)
-        pp1.loc1, pp1.loc2 = 4, 1  # inset corner 1 to origin corner 4 (would expect 1)
-        pp2.loc1, pp2.loc2 = 2, 2  # inset corner 3 to origin corner 2 (would expect 3)
-        plt.draw()               
+        # # Draw zoom in 
+        # zoom_in_factor = 2
+        # upscale_factor = 7.5
+        # ec = ".2"
+        # zoom_loc_x = (1000,1400)
+        # zoom_loc_y = (500,100)
+        # axins = zoomed_inset_axes(axarr[1,i], zoom_in_factor, loc = 'lower left')    
+        # axins.imshow(samples[i, 0, :, :], cmap = cmocean.cm.balance)
+        # axins.set_xlim(tuple(val // upscale_factor for val in zoom_loc_x))
+        # axins.set_ylim(tuple(val // upscale_factor for val in zoom_loc_y))
+        # plt.xticks(visible = False)
+        # plt.yticks(visible = False)
+        # _patch, pp1, pp2 = mark_inset(
+        #     axarr[1, i], 
+        #     axins, 
+        #     loc1 = 4, 
+        #     loc2 = 2, 
+        #     fc = "none", 
+        #     ec = ec, 
+        #     lw = 1.0, 
+        #     color = 'black'
+        # ) 
+        # #pp1.loc1, pp1.loc2 = 2, 3  # inset corner 2 to origin corner 3 (would expect 2)
+        # #pp2.loc1, pp2.loc2 = 4, 1  # inset corner 4 to origin corner 1 (would expect 4)
+        # pp1.loc1, pp1.loc2 = 4, 1  # inset corner 1 to origin corner 4 (would expect 1)
+        # pp2.loc1, pp2.loc2 = 2, 2  # inset corner 3 to origin corner 2 (would expect 3)
+        # plt.draw()               
 
     for i in range(ncol): 
         axarr[2, i].imshow(targets[i, 0, :, :], cmap = cmocean.cm.balance)
@@ -73,34 +73,34 @@ def plot_samples(
         axarr[2, i].set_yticks([])
         axarr[2, i].title.set_text("HR Ground Truth")                 
 
-        # Draw zoom in 
-        zoom_in_factor = 2
-        upscale_factor = 7.5
-        ec = ".2"
-        zoom_loc_x = (1000, 1400)
-        zoom_loc_y = (500, 100)
+        # # Draw zoom in 
+        # zoom_in_factor = 2
+        # upscale_factor = 7.5
+        # ec = ".2"
+        # zoom_loc_x = (1000, 1400)
+        # zoom_loc_y = (500, 100)
 
-        axins = zoomed_inset_axes(axarr[2,i], zoom_in_factor, loc = 'lower left')    
-        axins.imshow(targets[i, 0, :, :], cmap = cmocean.cm.balance)
-        axins.set_xlim(tuple(val // upscale_factor for val in zoom_loc_x))
-        axins.set_ylim(tuple(val // upscale_factor for val in zoom_loc_y))
-        plt.xticks(visible = False)
-        plt.yticks(visible = False)
-        _patch, pp1, pp2 = mark_inset(
-            axarr[2,i], 
-            axins, 
-            loc1 = 4, 
-            loc2 = 2, 
-            fc = "none", 
-            ec = ec, 
-            lw = 1.0, 
-            color = 'black'
-        )
-        #pp1.loc1, pp1.loc2 = 2, 3  # inset corner 2 to origin corner 3 (would expect 2)
-        #pp2.loc1, pp2.loc2 = 4, 1  # inset corner 4 to origin corner 1 (would expect 4)
-        pp1.loc1, pp1.loc2 = 4, 1  # inset corner 1 to origin corner 4 (would expect 1)
-        pp2.loc1, pp2.loc2 = 2, 2  # inset corner 3 to origin corner 2 (would expect 3)
-        plt.draw() 
+        # axins = zoomed_inset_axes(axarr[2,i], zoom_in_factor, loc = 'lower left')    
+        # axins.imshow(targets[i, 0, :, :], cmap = cmocean.cm.balance)
+        # axins.set_xlim(tuple(val // upscale_factor for val in zoom_loc_x))
+        # axins.set_ylim(tuple(val // upscale_factor for val in zoom_loc_y))
+        # plt.xticks(visible = False)
+        # plt.yticks(visible = False)
+        # _patch, pp1, pp2 = mark_inset(
+        #     axarr[2,i], 
+        #     axins, 
+        #     loc1 = 4, 
+        #     loc2 = 2, 
+        #     fc = "none", 
+        #     ec = ec, 
+        #     lw = 1.0, 
+        #     color = 'black'
+        # )
+        # #pp1.loc1, pp1.loc2 = 2, 3  # inset corner 2 to origin corner 3 (would expect 2)
+        # #pp2.loc1, pp2.loc2 = 4, 1  # inset corner 4 to origin corner 1 (would expect 4)
+        # pp1.loc1, pp1.loc2 = 4, 1  # inset corner 1 to origin corner 4 (would expect 1)
+        # pp2.loc1, pp2.loc2 = 2, 2  # inset corner 3 to origin corner 2 (would expect 3)
+        # plt.draw() 
 
     plt.tight_layout()
     plt.savefig(PATH + "/ddpm_sample_"+ str(epoch) + ".png")
