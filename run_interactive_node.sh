fc9adfed0cffc25e0492f3895cfe07a9f0399e25
paulpuren <ren.pu@northeastern.edu>
2025-08-13 11:39:44 -0700

    update run params

--- run_interactive_node.sh
+++ run_interactive_node.sh
@@ -1,19 +1,31 @@
 # salloc -N 1 -C gpu -q interactive -t 04:00:00 -G 4 -A m1516
 
-#export CUDA_VISIBLE_DEVICES=6,7;  python train.py --model UNetVIT --batch-size 18 --run-name UNetVIT --learning-rate 0.0002 --epochs 300
-
 module load python
 conda activate /pscratch/sd/p/puren93/miniforge3.bak-20250811-1035/envs/sft
+
+# python train.py \
+#     --model FLEX \
+#     --run_name FLEX \
+#     --optimizer 'adam' \
+#     --batch_size 12 \
+#     --learning_rate 1e-4 \
+#     --epochs 50 \
+#     --patch_size 256 \
+#     --stride 128 \
+#     --checkpoint_path '' \
+#     --is_T_fixed False \
 
 python train.py \
     --model FLEX \
     --run_name FLEX \
     --optimizer 'adam' \
-    --batch_size 12 \
-    --learning_rate 1e-4 \
-    --epochs 50 \
-    --patch_size 256 \
+    --batch_size 48 \
+    --learning_rate 3e-4 \
+    --epochs 200 \
+    --patch_size 128 \
     --stride 128 \
     --checkpoint_path '' \
-    --is_T_fixed True
+    --is_T_fixed False \
+    --data_name 'shanghai' \
+    --scratch_dir '/global/cfs/cdirs/m4633/puren/interp_dm/shanghai/shanghai.h5'
 
