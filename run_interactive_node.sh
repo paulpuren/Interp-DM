ca58983368a83072bc8b6db8cc16a5a5fc13c0de
paulpuren <ren.pu@northeastern.edu>
2025-08-27 16:04:48 -0700

    add more args for shanghai data

--- run_interactive_node.sh
+++ run_interactive_node.sh
@@ -3,6 +3,7 @@
 module load python
 conda activate /pscratch/sd/p/puren93/miniforge3.bak-20250811-1035/envs/sft
 
+# # run nskt
 # python train.py \
 #     --model FLEX \
 #     --run_name FLEX \
@@ -10,20 +11,22 @@ conda activate /pscratch/sd/p/puren93/miniforge3.bak-20250811-1035/envs/sft
 #     --batch_size 12 \
 #     --learning_rate 1e-4 \
 #     --epochs 50 \
-#     --patch_size 256 \
-#     --stride 128 \
+#     --patch_size 128 \
+#     --stride 64 \
 #     --checkpoint_path '' \
 #     --is_T_fixed False \
 
+# run shanghai
 python train.py \
     --model FLEX \
     --run_name FLEX \
     --optimizer 'adam' \
-    --batch_size 48 \
+    --batch_size 32 \
     --learning_rate 3e-4 \
-    --epochs 200 \
+    --epochs 300 \
     --patch_size 128 \
-    --stride 128 \
+    --total_interp_steps 5 \
+    --stride 64 \
     --checkpoint_path '' \
     --is_T_fixed False \
     --data_name 'shanghai' \
