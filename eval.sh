f02764915a17a8853ef05ff0b6cf8f625e1d6267
paulpuren <ren.pu@northeastern.edu>
2025-11-03 15:56:04 -0800

    update eval

--- eval.sh
+++ eval.sh
@@ -1,17 +1,17 @@
 # salloc -N 1 -C gpu -q interactive -t 01:00:00 -G 4 -A m1516
 
 module load python
-conda activate /pscratch/sd/p/puren93/miniforge3.bak-20250811-1035/envs/sft
+conda activate /pscratch/sd/p/puren93/conda_env/genai
 
 python eval_shanghai.py \
     --model FLEX \
     --data_name 'shanghai' \
     --re_num_id -1 \
-    --batch_size 24 \
-    --total_interp_steps 18 \
-    --total_interp_steps_train 10 \
+    --batch_size 16 \
+    --total_interp_steps 10 \
+    --total_interp_steps_train 20 \
     --optimizer 'adam' \
-    --learning_rate 3e-4 \
+    --learning_rate 1e-4 \
     --epochs 300 \
     --patch_size 128 \
     --stride 64 \
