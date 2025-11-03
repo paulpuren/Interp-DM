41994cfe8b59c32375864170ee11a28411ae1b93
paulpuren <ren.pu@northeastern.edu>
2025-11-03 15:58:09 -0800

    update run slurm code

--- run_slurm.sh
+++ run_slurm.sh
@@ -12,7 +12,8 @@
 #SBATCH --gpu-bind=none
 
 module load python
-conda activate /pscratch/sd/p/puren93/miniforge3/envs/sft
+conda activate /pscratch/sd/p/puren93/conda_env/genai
+
 
 # python train.py \
 #     --model UNetVIT \
@@ -25,14 +26,30 @@ conda activate /pscratch/sd/p/puren93/miniforge3/envs/sft
 #     --use_last_snapshot True \
 #     --num_pred_steps 10 \
 
+# python train.py \
+#     --model FLEX \
+#     --batch_size 12 \
+#     --run_name FLEX_refine_adam_1e-4_mlpr2 \
+#     --learning_rate 1e-4 \
+#     --epochs 200 \
+#     --patch_size 256 \
+#     --stride 128 \
+#     --use_last_snapshot True \
+#     --num_pred_steps 10 \
+#     --checkpoint_path ''
+
+# run sea temperature, flex
 python train.py \
     --model FLEX \
-    --batch_size 12 \
-    --run_name FLEX_refine_adam_1e-4_mlpr2 \
+    --run_name FLEX \
+    --optimizer 'adam' \
+    --batch_size 32 \
     --learning_rate 1e-4 \
-    --epochs 200 \
-    --patch_size 256 \
-    --stride 128 \
-    --use_last_snapshot True \
-    --num_pred_steps 10 \
-    --checkpoint_path ''
+    --epochs 60 \
+    --patch_size 128 \
+    --total_interp_steps 10 \
+    --stride 64 \
+    --checkpoint_path '' \
+    --is_T_fixed False \
+    --data_name 'sea_temp' \
+    --scratch_dir '/global/cfs/projectdirs/m4633/puren/interp_dm/sea_temp/'
