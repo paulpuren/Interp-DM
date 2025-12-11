e0a0fc479fd59ee361dd4fb32def4de6461ded51
paulpuren <ren.pu@northeastern.edu>
2025-12-10 23:45:40 -0800

    remove old nskt loader

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,5 +1,5 @@
 from .data_nskt import NSKT
 from .data_shanghai import Shanghai
-from .get_data import NSKT
+# from .get_data import NSKT
 from .data_era5_z500 import InputHandle
 from .data_sea_temp import InputHandle
